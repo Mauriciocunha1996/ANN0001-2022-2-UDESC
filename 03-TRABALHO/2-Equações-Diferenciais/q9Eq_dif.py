e3087b58828938479195f82738f82e870439f77c
Mauriciocunha1996 <mauricio.nascimento.cunha@gmail.com>
2022-12-10 06:24:23 -0300

    ANN

--- 03-TRABALHO/2-Equações-Diferenciais/q9Eq_dif.py
+++ 03-TRABALHO/2-Equações-Diferenciais/q9Eq_dif.py
@@ -1,48 +1,141 @@
-"""Considere o seguinte PVI
-y′=y(2−x)+x+1,y(x0)=y0,
-com x0=0.26939 e y0=1.68556. Use o método de Heun para estimar o valor da solução exata desse PVI nos pontos
-x1=0.31053, x2=0.35252, x3=0.3787, x4=0.44592, x5=0.50917, x6=0.5509, x7=0.60043, x8=0.6391, x9=0.69363, x10=0.75313, x11=0.79533, x12=0.84201, x13=0.87618, x14=0.92892, x15=0.988, x16=1.06191, x17=1.11304, x18=1.1463, x19=1.20006 e x20=1.23062."""
+import numpy as np
 
 
 
-def heun(f,x0,y0,h,n,x_values):
-    for k in range(1,n):
-        m1 = f(x0,y0)
-        m2 = f(x0 + h, y0 + h * m1)
-        y0 += h * (m1+m2) / 2
-        h = x_values[k] - x_values[k-1]
-        x0 = x_values[k-1]
-        yield[x0,y0]
+def euler(f, x0, y0, h, n):
+    vals = []
+    for k in range(n):
+        y0 += h*f(x0, y0)
+        x0 += h
+        vals.append([x0, y0])
+    return vals
 
-    m1 = f(x0,y0)
-    m2 = f(x0 + h, y0 + h * m1)
-    y0 += h * (m1+m2) / 2
-    h = x_values[n-1] - x_values[n-2]
-    x0 = x_values[k-1]
-    yield[x0,y0]
+
+# b = 1
+def euler_mid(f, x0, y0, h, n):
+    vals = []
+    for _ in range(n):
+        m1 = f(x0, y0)
+        m2 = f(x0 + h / 2, y0 + (h/2) * m1)
+        y0 += h*m2
+        x0 += h
+        vals.append([x0, y0])
+    return vals
+
+
+# b = 1/2
+def heun(f, x0, y0, h, n):
+    vals = []
+    for _ in range(n):
+        m1 = f(x0, y0)
+        m2 = f(x0 + h, y0 + h*m1)
+        y0 += h*(m1+m2)/2
+        x0 += h
+        vals.append([x0, y0])
+    return vals
+
+
+# b = 2/3
+# def ralston(f, x0, y0, h, n):
+#     vals = []
+#     for _ in range(n):
+#         m1 = f(x0, y0)
+#         m2 = f(x0 + 0.75*h, y0 + 0.75*h*m1)
+#         y0 = h*(m1 + 2*m2)/3
+#         x0 += h
+#         vals.append([x0, y0])
+#     return vals
+
+
+# padrao = euler_mid
+def rk2(f, x0, y0, h, n, b=1.0):
+    # b = 1 => metodo = euler_mid
+    # b = 1/2 => metodo = heun
+    # b = 2/3 => metodo = ralston
+    vals = []
+    a = 1-b
+    p = 1/(2*b)
+    q = p
+    for _ in range(n):
+        m1 = f(x0, y0)
+        m2 = f(x0 + p*h, y0 + q*h*m1)
+        y0 += (a*m1 + b*m2)*h
+        x0 += h
+        vals.append([x0, y0])
+    return vals
+
+
+def rk2_h_variavel(f, x0, y0, n, b, x_values):
+    # b = 1 => metodo = euler_mid
+    # b = 1/2 => metodo = heun
+    # b = 2/3 => metodo = ralston
+    vals = []
+    a = 1-b
+    p = 1/(2*b)
+    q = p
+    for i in range(n):
+        if i == 0:
+            h = x_values[0] - x0
+        else:
+            h = x_values[i] - x_values[i-1]
+        m1 = f(x0, y0)
+        m2 = f(x0 + p*h, y0 + q*h*m1)
+        y0 += (a*m1 + b*m2)*h
+        x0 += h
+        vals.append([x0, y0])
+    return vals
+
+
+def diff(a, b):
+    return sum((ai - bi)**2 for ai, bi in zip(a, b))
+
+
+def f(x, y):
+    return y * (2 - x) + x + 1
+
+
+def g(t, i):
+    c = 0.2041
+    r = 1.0748 
+    l = 1.5355
+
+    # considerando a função e(t) = e^(-e_value*pi*t)*sin(2*t-pi)
+    # se e^(-0.0619*pi*t) => e_value = 0.0619
+    e_value = 0.0598
+
+    def e(t):
+        return np.exp(-e_value*np.pi*t)*np.sin(2*t-np.pi)
+
+    def e_(t):
+        return np.exp(-e_value*np.pi*t)*(2*np.cos(np.pi-2*t)+e_value*np.pi*np.sin(np.pi-2*t))
+
+    def e__(t):
+        return np.exp(-e_value*np.pi*t)*((4-pow(e_value, 2)*pow(np.pi, 2))*np.sin(np.pi-2*t)-4*e_value*np.pi*np.cos(np.pi-2*t))
+
+    return c*e__(t) + (1/r)*e_(t) + (1/l)*e(t)
+
 
 if __name__ == '__main__':
-    #usar o wolpram alpha como resolvedor de equacoes diferencias.
-
-    def f(x,y):
-        func = y * (2 - x) + x + 1
-        return func
 
     
-    x0 = 0.26939
-    y0 = 1.68556
-    x_values = [0.31053, 0.35252, 0.3787, 0.44592, 0.50917, 0.5509, 0.60043, 0.6391, 0.69363, 0.75313, 0.79533, 0.84201, 0.87618, 0.92892, 0.988, 1.06191, 1.11304, 1.1463, 1.20006, 1.23062]
-    n = 20 # nmro de iteracoes
+    n = 20
+    b = 1/2
+    x0 = 1.17657
+    y0 = 1.98153
+    x_values = [1.21261, 1.26176, 1.29617, 1.34967, 1.40841, 1.46799, 1.51324, 1.56537, 1.58429, 1.67078, 1.70066, 1.7639, 1.79934, 1.86914, 1.91491, 1.9714, 1.99267, 2.03214, 2.10721, 2.16479]
+    # metodo2 = euler_mid(f, x0, y0, h, n)
+    #metodo3 = heun(f, x0, y0, h, n)
+    # metodo4 = ralston(f, x0, y0, h, n)
+    # metodo5 = rk2(g, x0, y0, h, n, b)
+    metodo5 = rk2_h_variavel(f, x0, y0, n, b, x_values)
 
-    h = x_values[0] - x0
+    indice = [i for i in range(n)]
+    lista_x, lista_y = zip(*metodo5)
 
-    r3 = heun(f,x0,y0, h,n,x_values)
-    x3,y3 = zip(*r3)
+    for i, xi, yi in zip(indice, lista_x, lista_y):
+        # print(f'x{i} = {xi} => y{i} = {yi}')
+        print(f'{yi},', end='')
 
-    valores = str(y3)[1:-1] 
-    print(valores)
+    #plt.scatter(lista_x, lista_y)
 
-    #ele so vai pedir o valor das coordenadas y.
-    
-
-   
+   # plt.savefig('edo.png')
